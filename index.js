370730f0c9238fc209151e9c33ee8d99998f6d35
jAllanDev <juan.alves@fatec.sp.gov.br>
2025-02-27 17:51:11 -0300

    Mudanças na estrutura dos arquivos

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
  // Essentials
 
- import { MESSAGE_ID, CHANNEL_ID, roleId, emoji } from "./config.json";
+import { MESSAGE_ID, CHANNEL_ID, roleId, emoji } from "./config.json";
+import "./events/reaction.js";
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -26,64 +27,6 @@ client.once("ready", () => {
     console.log(`✅ Bot conectado como ${client.user.tag}`);
   });
   
-  import { Events } from "discord.js";
-
- // Mensagem fixa
-
-client.once(Events.ClientReady, async () => {
-    console.log(`Bot iniciado como ${client.user.tag}`);
-
-    try {
-        const channel = await client.channels.fetch(CHANNEL_ID);
-        if (!channel) return console.error("Canal não encontrado.");
-
-        const message = await channel.messages.fetch(MESSAGE_ID);
-        console.log("Mensagem encontrada! Adicionando reação...");
-
-        await message.react(emoji);
-
-        console.log("Reação adicionada com sucesso!");
-    } catch (error) {
-        console.error("Erro ao buscar a mensagem fixa:", error);
-    }
-});
-
- // Dando cargo
-
-client.on(Events.MessageReactionAdd, async (reaction, user) => {
-    if (user.bot) return; // Ignora reações de bots
-
-    try {
-        if (reaction.emoji.name === emoji) {
-            const member = await reaction.message.guild.members.fetch(user.id);
-            await member.roles.add(roleId);
-            console.log(`Cargo adicionado para ${user.tag}`);
-
-             // Envia DM para o usuário
-            await user.send("🎉 Você recebeu o cargo com sucesso! Bem-vindo! Caso ainda não tenha lido, verifique a INTRODUÇÃO no servidor.");
-        }
-    } catch (error) {
-        console.error("Erro ao adicionar cargo:", error);
-    }
-});
-
-client.on(Events.MessageReactionRemove, async (reaction, user) => {
-    if (user.bot) return;
-
-    try {
-        if (reaction.emoji.name === emoji) {
-            const member = await reaction.message.guild.members.fetch(user.id);
-            await member.roles.remove(roleId);
-            console.log(`Cargo removido de ${user.tag}`);
-        }
-    } catch (error) {
-        console.error("Erro ao remover cargo:", error);
-    }
-});
-
-
-
-
 
  // Server sided
 client.login(process.env.DISCORD_TOKEN)
